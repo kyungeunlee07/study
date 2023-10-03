2081c5971677e11b1de6d8a7c0c12d2aeb77bdbd
ggamD00 <jaehuek@naver.com>
2023-10-03 17:40:22 +0900

    Day03

--- 03 자료구조/03-2 구간 합/Q005_jaehyeok.py
+++ 03 자료구조/03-2 구간 합/Q005_jaehyeok.py
@@ -1,3 +1,30 @@
 # 005 나머지 합 구하기 https://www.acmicpc.net/problem/10986
 
+import sys
+input = sys.stdin.readline
 
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+S = [0] * n
+C = [0] * m
+S[0] = A[0]
+answer = 0
+
+for i in range(1, n):
+    S[i] = S[i-1] + A[i]    # 합 배열 저장
+
+for i in range(n):
+    remainder = S[i] % m    # 합 배열의 모든 값에 %연산 수행
+    if remainder == 0:      # 0~i 까지의 구간 합 자체가 0일 때 정답에 더하기
+        answer += 1
+    C[remainder] += 1       # 나머지가 같은 인덱스의 개수 값 증가시키기
+
+for i in range(m):
+    # 나머지가 같은 인덱스 중 2개를 뽑는 경우의 수를 더하기 (조합)
+    if C[i] > 1:
+        answer += (C[i] * (C[i]-1) // 2)
+print(answer)
+
+'''
+(S[i] % m)의 값과 (S[j] % m)의 값이 같다면 ((S[i] - S[j]) % m)은 0이다.
+'''
--- 03 자료구조/03-3 투 포인터/Q006_jaehyeok.py
+++ 03 자료구조/03-3 투 포인터/Q006_jaehyeok.py
@@ -1,3 +1,24 @@
 # 006 연속된 자연수의 합 구하기 https://www.acmicpc.net/problem/2018
 
+n = int(input())
+count = 1   # 1로 시작하는 이유는 자기 자신을 포함하고 시작하기 때문이다.
+start_index = 1
+end_index = 1
+sum = 1
 
+while end_index != n:
+    if sum == n:
+        count += 1
+        end_index += 1
+        sum += end_index
+    elif sum > n:
+        sum -= start_index
+        start_index += 1
+    else:
+        end_index += 1
+        sum += end_index
+print(count)
+
+'''
+투 포인터 이동 원칙을 잘 생각 해내야 한다.
+'''
